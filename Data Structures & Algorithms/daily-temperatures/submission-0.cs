7928055cbe91f467116126474660fcb28e0700ac
Nikoloz Amashukeli <nikolozamashukeli24@gmail.com>
2026-06-15 23:54:56 +0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+
+public int[] DailyTemperatures(int[] temperatures) {
+        Stack<int> stack = new();
+        int[] result = new int[temperatures.Length];
+
+for(int i = 0; i < temperatures.Length; i++)
+{
+
+while(stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
+{
+
+int poppedIndex = stack.Pop();
+result[poppedIndex] = i - poppedIndex;
+}
+
+stack.Push(i);
+}
+
+return result;
+}
+
+}
